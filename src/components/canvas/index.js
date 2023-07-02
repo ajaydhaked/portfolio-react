f9be8e2e577220aaa8a0f687586a83aea233d652
Ajay Kumar Dhakar <ajaydhaker2002@kgpian.iitkgp.ac.in>
2023-07-02 19:14:06 +0530

    ParticlesImportPath

--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -2,5 +2,5 @@ import EarthCanvas from "./Earth";
 import BallCanvas from "./Ball";
 import Halomodel from "./Computers";
 import StarsCanvas from "./Stars";
-import ParticlesContainer from "./particles";
+import ParticlesContainer from "./Particles";
 export { EarthCanvas, BallCanvas, Halomodel, StarsCanvas,ParticlesContainer };
