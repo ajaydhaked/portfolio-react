41ad187c83c0056d47b59c0445aafe3dd9fdffd3
Ajay Kumar Dhakar <ajaydhaker2002@kgpian.iitkgp.ac.in>
2023-07-03 10:31:48 +0530

    HeroSectionTextChanged

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
             <div>
               <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]' >Ajay</span></h1>
               <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-                I'm a full stack developer, Competitive <br className='sm:block hidden' /> Programmer and a Tech Enthusiast.
+                I'm a Second year Undergraduate <br className='sm:block hidden' />student of the Department of <br className='sm:block hidden' /><span className='text-red-500'>Computer Science and Engineering</span> <br className='sm:block hidden' />at <span className='text-red-500'>IIT Kharagpur.</span>
               </p>
             </div>
       </div>
